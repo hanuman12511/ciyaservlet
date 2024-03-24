3e310eec06feb1e62fdaa17518051c17957a970d
hanuman12511 <er.hanumansaini@gmail.com>
2024-03-24 18:30:17 +0530

    servlet

---
+++ WEB-INF/classes/pro/HomePage.java
@@ -0,0 +1,19 @@
+package pro;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class HomePage extends HttpServlet{
+    public void service(HttpServletRequest request,
+    HttpServletResponse response)
+    throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+  
+   out.println("HomeScreen");
+  
+   
+  }
+  }
